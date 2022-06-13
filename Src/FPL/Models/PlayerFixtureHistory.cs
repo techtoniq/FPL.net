e8f012d8083610260ad8207117803107b7593a38
Matt Daniels <matt@techtoniq.com>
2022-06-13 19:52:27 +0100

    Fix code duplication

--- Src/FPL/Models/PlayerFixtureHistory.cs
+++ Src/FPL/Models/PlayerFixtureHistory.cs
@@ -1,6 +1,6 @@
 ﻿namespace FPL.Models
 {
-    public record PlayerFixtureHistory
+    public record PlayerFixtureHistory : PlayerStats
     {
         public int Element { get; init; } = default!;
         public int Fixture { get; init; } = default!;
@@ -11,23 +11,6 @@
         public int TeamHScore { get; init; } = default!;
         public int TeamAScore { get; init; } = default!;
         public int Round { get; init; } = default!;
-        public int Minutes { get; init; } = default!;
-        public int GoalsScored { get; init; } = default!;
-        public int Assists { get; init; } = default!;
-        public int CleanSheets { get; init; } = default!;
-        public int GoalsConceded { get; init; } = default!;
-        public int OwnGoals { get; init; } = default!;
-        public int PenaltiesSaved { get; init; } = default!;
-        public int PenaltiesMissed { get; init; } = default!;
-        public int YellowCards { get; init; } = default!;
-        public int RedCards { get; init; } = default!;
-        public int Saves { get; init; } = default!;
-        public int Bonus { get; init; } = default!;
-        public int BPS { get; init; } = default!;
-        public string Influence{ get; init; } = default!;
-        public string Creativity { get; init; } = default!;
-        public string Threat { get; init; } = default!;
-        public string ICTIndex { get; init; } = default!;
         public int Value { get; init; } = default!;
         public int TransfersBalance { get; init; } = default!;
         public int Selected { get; init; } = default!;
---
+++ Src/FPL/Models/PlayerStats.cs
@@ -0,0 +1,23 @@
+﻿namespace FPL.Models
+{
+    public abstract record PlayerStats
+    {
+        public int Minutes { get; init; } = default!;
+        public int GoalsScored { get; init; } = default!;
+        public int Assists { get; init; } = default!;
+        public int CleanSheets { get; init; } = default!;
+        public int GoalsConceded { get; init; } = default!;
+        public int OwnGoals { get; init; } = default!;
+        public int PenaltiesSaved { get; init; } = default!;
+        public int PenaltiesMissed { get; init; } = default!;
+        public int YellowCards { get; init; } = default!;
+        public int RedCards { get; init; } = default!;
+        public int Saves { get; init; } = default!;
+        public int Bonus { get; init; } = default!;
+        public int BPS { get; init; } = default!;
+        public string Influence { get; init; } = default!;
+        public string Creativity { get; init; } = default!;
+        public string Threat { get; init; } = default!;
+        public string ICTIndex { get; init; } = default!;
+    }
+}
